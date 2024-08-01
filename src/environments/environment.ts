1134cf64e23992274fbf2b4ce37112969cab7d8c
hemashree135 <164997502+hemashree135@users.noreply.github.com>
2024-08-01 15:57:23 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+  apiKey: "AIzaSyA0845raQpTeMpOQYtCCNecJqvxcYljiuM",
+  authDomain: "friendlychat-9c682.firebaseapp.com",
+  projectId: "friendlychat-9c682",
+  storageBucket: "friendlychat-9c682.appspot.com",
+  messagingSenderId: "990539539744",
+  appId: "1:990539539744:web:c192d01dfc06826c8e6345"
   },
 };
